6f8b4d83f280090c1534e972e1acd4f6faf14d16
Scott Hunter <scott.k.hunter@gmail.com>
2012-03-12 00:55:34 -0400

    Visual changes.  Store report information in an object so that the template has full control over layout.  Group values by stage of the pipeline.

--- webglreport.js
+++ webglreport.js
@@ -36,19 +36,30 @@ $(function() {
     });
 
     var template = _.template($("#reportTemplate").html());
-    var report = [
-        ["Platform", navigator.platform],
-        ["Browser User Agent", navigator.userAgent]
-    ];
+    var report = {
+        platform: navigator.platform,
+        userAgent: navigator.userAgent
+    };
+
+    function getExtensionUrl(extension) {
+        //special cases
+        if (extension === 'WEBKIT_lose_context') {
+            extension = 'WEBGL_lose_context';
+        }
+        else if (extension === 'WEBKIT_WEBGL_compressed_textures') {
+            extension = '';
+        }
+        extension = extension.replace(/^WEBKIT_/, '');
+        extension = extension.replace(/EXT_/, '');
+
+        return 'http://www.khronos.org/registry/webgl/extensions/' + extension;
+    }
 
     function renderReport(header) {
-        _.each(report, function(item) {
-            if (item.length > 1 && item[0].length > 0) {
-                item[0] += ":";
-            }
-        });
-
-        $("#output").html(header + template({ report: report }));
+        $("#output").html(header + template({
+            report: report,
+            getExtensionUrl: getExtensionUrl
+         }));
     }
 
     if (!gl) {
@@ -60,49 +71,34 @@ $(function() {
         return "[" + value[0] + ", " + value[1] + "]";
     }
 
-    report.push(
-        ["Context Name", contextName],
-        ["GL Version", gl.getParameter(gl.VERSION)],
-        ["Shading Language Version", gl.getParameter(gl.SHADING_LANGUAGE_VERSION)],
-        ["Vendor", gl.getParameter(gl.VENDOR)],
-        ["Renderer", gl.getParameter(gl.RENDERER)],
-
-        ["Pixel Depths"],
-        ["Red Bits", gl.getParameter(gl.RED_BITS)],
-        ["Green Bits", gl.getParameter(gl.GREEN_BITS)],
-        ["Blue Bits", gl.getParameter(gl.BLUE_BITS)],
-        ["Alpha Bits", gl.getParameter(gl.ALPHA_BITS)],
-        ["Depth Bits", gl.getParameter(gl.DEPTH_BITS)],
-        ["Stencil Bits", gl.getParameter(gl.STENCIL_BITS)],
-
-        ["Implementation Dependent States"],
-        ["Max. Render Buffer Size", gl.getParameter(gl.MAX_RENDERBUFFER_SIZE)],
-        ["Max. Combined Texture Image Units", gl.getParameter(gl.MAX_COMBINED_TEXTURE_IMAGE_UNITS)], // min: 8
-        ["Max. Cube Map Texture Size", gl.getParameter(gl.MAX_CUBE_MAP_TEXTURE_SIZE)], // min: 16
-        ["Max. Fragment Uniform Vectors", gl.getParameter(gl.MAX_FRAGMENT_UNIFORM_VECTORS)], // min: 16
-        ["Max. Texture Image Units", gl.getParameter(gl.MAX_TEXTURE_IMAGE_UNITS)], // min: 8
-        ["Max. Texture Size", gl.getParameter(gl.MAX_TEXTURE_SIZE)], // min: 64
-        ["Max. Varying Vectors", gl.getParameter(gl.MAX_VARYING_VECTORS)], // min: 8
-        ["Max. Vertex Attributes", gl.getParameter(gl.MAX_VERTEX_ATTRIBS)], // min: 8
-        ["Max. Vertex Texture Image Units", gl.getParameter(gl.MAX_VERTEX_TEXTURE_IMAGE_UNITS) || "0"], // min: 0
-        ["Max. Vertex Uniform Vectors", gl.getParameter(gl.MAX_VERTEX_UNIFORM_VECTORS)], // min: 128
-        ["Aliased Line Width Range", describeRange(gl.getParameter(gl.ALIASED_LINE_WIDTH_RANGE))], // must include the value 1
-        ["Aliased Point Size Range", describeRange(gl.getParameter(gl.ALIASED_POINT_SIZE_RANGE))], // must include the value 1
-        ["Max. Viewport Dimensions", describeRange(gl.getParameter(gl.MAX_VIEWPORT_DIMS))],
-
-        ["Supported Extensions"]
-    );
-
-    var extensions = gl.getSupportedExtensions();
-    if (extensions.length > 0) {
-        report = report.concat(_.map(extensions, function(extension) {
-            return ["", extension];
-        }));
-    } else {
-        report.push(
-            ["", "No extensions were found."]
-        );
-    }
+    report = _.extend(report, {
+       contextName: contextName,
+       glVersion: gl.getParameter(gl.VERSION),
+       shadingLanguageVersion: gl.getParameter(gl.SHADING_LANGUAGE_VERSION),
+       vendor: gl.getParameter(gl.VENDOR),
+       renderer: gl.getParameter(gl.RENDERER),
+       redBits: gl.getParameter(gl.RED_BITS),
+       greenBits: gl.getParameter(gl.GREEN_BITS),
+       blueBits: gl.getParameter(gl.BLUE_BITS),
+       alphaBits: gl.getParameter(gl.ALPHA_BITS),
+       depthBits: gl.getParameter(gl.DEPTH_BITS),
+       stencilBits: gl.getParameter(gl.STENCIL_BITS),
+       maxRenderBufferSize: gl.getParameter(gl.MAX_RENDERBUFFER_SIZE),
+       maxCombinedTextureImageUnits: gl.getParameter(gl.MAX_COMBINED_TEXTURE_IMAGE_UNITS), // min: 8
+       maxCubeMapTextureSize: gl.getParameter(gl.MAX_CUBE_MAP_TEXTURE_SIZE), // min: 16
+       maxFragmentUniformVectors: gl.getParameter(gl.MAX_FRAGMENT_UNIFORM_VECTORS), // min: 16
+       maxTextureImageUnits: gl.getParameter(gl.MAX_TEXTURE_IMAGE_UNITS), // min: 8
+       maxTextureSize: gl.getParameter(gl.MAX_TEXTURE_SIZE), // min: 64
+       maxVaryingVectors: gl.getParameter(gl.MAX_VARYING_VECTORS), // min: 8
+       maxVertexAttributes: gl.getParameter(gl.MAX_VERTEX_ATTRIBS), // min: 8
+       maxVertexTextureImageUnits: gl.getParameter(gl.MAX_VERTEX_TEXTURE_IMAGE_UNITS) || 0, // min: 0
+       maxVertexUniformVectors: gl.getParameter(gl.MAX_VERTEX_UNIFORM_VECTORS), // min: 128
+       aliasedLineWidthRange: describeRange(gl.getParameter(gl.ALIASED_LINE_WIDTH_RANGE)), // must include the value 1
+       aliasedPointSizeRange: describeRange(gl.getParameter(gl.ALIASED_POINT_SIZE_RANGE)), // must include the value 1
+       maxViewportDimensions: describeRange(gl.getParameter(gl.MAX_VIEWPORT_DIMS)),
+       extensions: gl.getSupportedExtensions()
+    });
+//            ["", "No extensions were found."]
 
     renderReport($("#webglSupportedTemplate").html());
 });
